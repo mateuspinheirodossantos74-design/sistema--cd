6c120ecd58c3e4e18287f618c9b4d3830596268e
mateuspinheirodossantos74-design <mateuspinheirodossantos74@gmail.com>
2026-04-02 23:51:27 -0300

    Atualizar o limpar_tabela.py

--- limpar_tabela.py
+++ limpar_tabela.py
@@ -1,13 +1,4 @@
-import mysql.connector
 
-# Conexão
-conn = mysql.connector.connect(
-    host="gondola.proxy.rlwy.net",
-    user="root",
-    password="NWyoLjEbDJydymKDvQHxQhzNwdJkAMuH",
-    database="railway",
-    port=25644
-)
 
 cursor = conn.cursor()
 
