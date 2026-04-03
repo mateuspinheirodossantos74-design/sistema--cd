da4c270df64aa5b66108d5b1e1c08b8674d234f4
mateuspinheirodossantos74-design <mateuspinheirodossantos74@gmail.com>
2026-04-02 23:49:43 -0300

    Atualizar o adicionar_coluna.py

--- adicionar_coluna.py
+++ adicionar_coluna.py
@@ -1,11 +1,5 @@
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host="gondola.proxy.rlwy.net",
-    user="root",
-    password="NWyoLjEbDJydymKDvQHxQhzNwdJkAMuH",
-    database="railway",
-    port=25644
 )
 cursor = conn.cursor()
 
